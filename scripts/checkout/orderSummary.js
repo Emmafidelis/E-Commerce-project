23ea6d711e493c61134719d1e487055cdb1e5d0a
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-06-14 11:17:15 +0300

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,6 +2,7 @@ import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 export function renderOrderSummary() {
@@ -101,6 +102,8 @@ export function renderOrderSummary() {
 
     const container = document.querySelector(`.js-cart-item-${productId}`)
     container.remove();
+
+    renderPaymentSummary();
   })
   })
 
@@ -109,6 +112,7 @@ export function renderOrderSummary() {
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId)
     renderOrderSummary();
+    renderPaymentSummary();
   })
   })
 }
